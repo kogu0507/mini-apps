3f5aa151f7dc0784ec6db79813abbdec3ccf2be6
kogu0507 <kogu0507@gmail.com>
2025-06-13 14:46:40 +0900

    本番用URLを設定 jsDelivr

--- chord-type-dictation-mini/modules/tonejs-loader.mjs
+++ chord-type-dictation-mini/modules/tonejs-loader.mjs
@@ -1,6 +1,7 @@
 // modules/tonejs-loader.mjs
 // import { loadScript } from './library-loader.mjs'; // 開発用
 import { loadScript } from 'https://cdn.jsdelivr.net/gh/kogu0507/mini-apps@main/chord-type-dictation-mini/module/library-loader.min.mjs'; // 本番用
+
 /**
  * Tone.jsをロードします。
  * @returns {Promise<void>}
